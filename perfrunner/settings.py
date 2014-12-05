fb881ef5eedfef2c487a7aa46429954375c6a0ae
Dave Rigby <daver@couchbase.com>
2014-12-05 11:49:40 +0000

    Add 'defragmenter_enabled' to set of configurable bucket props

--- perfrunner/settings.py
+++ perfrunner/settings.py
@@ -304,6 +304,7 @@ class BucketSettings(object):
     REPLICA_INDEX = 0
     EVICTION_POLICY = 'valueOnly'  # alt: fullEviction
     EXPIRY_PAGER_SLEEP_TIME = -1
+    DEFRAGMENTER_ENABLED = -1
 
     def __init__(self, options):
         self.password = options.get('password', self.PASSWORD)
@@ -325,6 +326,9 @@ class BucketSettings(object):
         )
         self.eviction_policy = \
             options.get('eviction_policy', self.EVICTION_POLICY)
+
+        self.defragmenter_enabled = options.get('defragmenter_enabled',
+                                                self.DEFRAGMENTER_ENABLED)
 
         self.threads_number = options.get('threads_number')  # 2.x
 
